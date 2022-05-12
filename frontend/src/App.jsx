f48bacdbb1ddbed47d19674a99ba6ba56f0b28b4
MORETTE Valentin <morette.valentin@gmail.com>
2022-05-12 12:27:18 +0200

    base react and create route/pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import Home from "@pages/Home";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Compost from "./pages/Compost";
+import Map from "./pages/Map";
+import Panel from "./pages/Panel";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="map" element={<Map />} />
+        <Route path="compost" element={<Compost />} />
+        <Route path="panel" element={<Panel />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Acceuil</Link>
+          </li>
+          <li>
+            <Link to="map">Map</Link>
+          </li>
+          <li>
+            <Link to="compost">compost</Link>
+          </li>
+          <li>
+            <Link to="panel">suivi</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
