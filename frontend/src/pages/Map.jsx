2e2724f6f12195c4dd6a76b1f418c98804f32427
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-05-12 17:25:34 +0200

    install leaflet and setup it into map

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,3 +1,22 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 export default function Map() {
-  return <p>Map</p>;
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: "50vh", width: "80vw" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
