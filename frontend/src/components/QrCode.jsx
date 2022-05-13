b1397f82aa70e74acc3e039fe7d113d0d4cd75a1
MORETTE Valentin <morette.valentin@gmail.com>
2022-05-13 08:25:01 +0200

    hover badge and move on home

--- frontend/src/components/QrCode.jsx
+++ frontend/src/components/QrCode.jsx
@@ -5,10 +5,15 @@ import { Link } from "react-router-dom";
 export default function QrCode() {
   return (
     <>
+      <div className="trichecentre">
+        <Link to="/">
+          <img className="logoComp" src="src/assets/logo1.png" alt="logo" />
+        </Link>
+      </div>
       <div className="divcam">
         <Webcam className="cam" width={300} />
       </div>
-      <Link to="/home">
+      <Link to="home">
         <div className="hitbox" />
       </Link>
     </>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,55 +12,76 @@ export default function Home() {
         />
       </Link>
       <div className="top-home">
-        <Link to="/panel">
-          <img src="/src/assets/147142.png" alt="avatar" className="avatar" />{" "}
+        <Link to="panel">
+          <img src="/src/assets/147142.png" alt="avatar" className="avatar" />
         </Link>
         <progress max="100" value="75" />
         <p>Maitre du Compostage</p>
       </div>
-      <div className="badgecheck">
-        <img src="src/assets/badge/pigeon.png" alt="badge pigeon" />
-        <img src="src/assets/badge/lune.png" alt="badge lune" />
-        <img src="src/assets/badge/feuille.png" alt="badge feuille" />
-      </div>
+      <Link to="panel">
+        <div className="badgecheck">
+          <span className="hovertext" data-hover="Atteint le niveau 2">
+            <img src="src/assets/badge/poussin.png" alt="badge poussin" />
+          </span>
+          <span className="hovertext" data-hover="Compost la nuit !">
+            <img src="src/assets/badge/lune.png" alt="badge lune" />
+          </span>
+          <span className="hovertext" data-hover="Premier Compost">
+            <img src="src/assets/badge/feuille.png" alt="badge feuille" />
+          </span>
+        </div>
+      </Link>
       <img
         src="src/assets/badge/badgecheck.png"
         alt="badge check"
         className="imgbadgecheck"
       />
       <div className="badgenocheck">
-        <img src="src/assets/badge/poussin.png" alt="badge poussin" />
+        <span className="hovertext" data-hover="Atteint le niveau 5">
+          <img src="src/assets/badge/pigeon.png" alt="badge pigeon" />
+        </span>
         <img src="src/assets/badge/hibou.png" alt="badge hibou" />
-        <img src="src/assets/badge/arbre.png" alt="badge arbre" />
+        <span className="hovertext" data-hover="5 compost (4/5)">
+          <img src="src/assets/badge/arbre.png" alt="badge arbre" />
+        </span>
       </div>
       <img
         src="src/assets/badge/badgenocheck.png"
         alt="badge no check"
         className="imgbadgenocheck"
       />
-      <p>Historique</p>
-      <div className="histoirearray">
-        <table>
-          <tr>
-            <td>Date</td>
-            <td>Quantités</td>
-            <td>Lieu</td>
-          </tr>
-          <tr>
-            <td> 01/05 </td>
-            <td> 350 G </td>
-            <td> REIMS </td>
-          </tr>
-          <tr>
-            <td>11/03</td>
-            <td>800 G</td>
-            <td>REIMS</td>
-          </tr>
-        </table>
+      <div className="histo">
+        <p className="phisto">Historique</p>
+        <div className="histoirearray">
+          <table>
+            <tr>
+              <th>Date</th>
+              <th>Quantités</th>
+              <th>Lieu</th>
+            </tr>
+            <tr>
+              <td> 01/05 </td>
+              <td> 350 G </td>
+              <td> REIMS </td>
+            </tr>
+            <tr>
+              <td>21/03</td>
+              <td>800 G</td>
+              <td>REIMS</td>
+            </tr>
+            <tr>
+              <td>30/01</td>
+              <td>600 G</td>
+              <td>REIMS</td>
+            </tr>
+          </table>
+        </div>
       </div>
-      <p>Où nous trouver ?</p>
-      <div className="trichecentre2">
-        <Map />
+      <div className="mapping">
+        <p className="pmapping">Où nous trouver ?</p>
+        <div className="trichecentre2">
+          <Map />
+        </div>
       </div>
     </>
   );
